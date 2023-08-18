4ee518bb98c1b3deba37c096dfd1b0bc6bf14f38
Caio Cabral <ocaiocabral@gmail.com>
2023-08-18 14:46:58 -0400

    feat: throwing error

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import fs from 'fs';
 import chalk from "chalk";
 
+function handleError(error){
+  throw new Error(chalk.red((error.code, "Something went wrong")));
+}
+
 function getFile(filePath){
   const encoding = "utf-8";
-  fs.readFile(filePath, encoding, (_, text) => {
+  fs.readFile(filePath, encoding, (error, text) => {
+    error && handleError(error);
     console.log(chalk.green(text))
   })
 
 }
 
-getFile('./archives/text.md');
+getFile('./aarchives/text.md');
