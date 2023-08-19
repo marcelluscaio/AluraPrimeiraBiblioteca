7d9da9924ce8596c9dc76de1ca0e4e7f9bcbd8f2
Caio Cabral <ocaiocabral@gmail.com>
2023-08-19 18:52:43 -0400

    feat: print file content and path

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,31 @@
 import fs from 'fs';
 import getFile from "./index.js";
+import chalk from "chalk";
 
 const path = process.argv[2];
 
-async function showResult(path){
+async function getResult(path){
+  try{
+    fs.lstatSync(path);
+  } catch(error){
+    if(error.code === 'ENOENT'){
+      console.log("Path doesnt exist");
+      return
+    }
+  }
+
   if(fs.lstatSync(path).isFile()){
     const result = await getFile(path);
+    console.log(chalk.green(path));
     console.log(result);
   } else if(fs.lstatSync(path).isDirectory()){
     const files = await fs.promises.readdir(path);
-    files.forEach(file => showResult(path.concat('/', file)))
+    files.forEach(file => getResult(path.concat('/', file)))
   }
   
 }
 
-showResult(path);
+getResult(path);
 
 
 
