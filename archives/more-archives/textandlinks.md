57e5f14af8a3966917198ee605a1fb13be632091
Caio Cabral <ocaiocabral@gmail.com>
2023-08-19 18:31:26 -0400

    feat: cli looks for path, if it is a file it reads it, if it is a directory it opens it, even if there are other directories within it

---
+++ archives/more-archives/textandlinks.md
@@ -0,0 +1,1 @@
+[more-text](https://mywebsite.doesntwork.com)
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,17 @@
+import fs from 'fs';
 import getFile from "./index.js";
 
 const path = process.argv[2];
 
 async function showResult(path){
-  const result = await getFile(path);
-  console.log(result);
+  if(fs.lstatSync(path).isFile()){
+    const result = await getFile(path);
+    console.log(result);
+  } else if(fs.lstatSync(path).isDirectory()){
+    const files = await fs.promises.readdir(path);
+    files.forEach(file => showResult(path.concat('/', file)))
+  }
+  
 }
 
 showResult(path);
